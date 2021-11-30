5bce90ce8941c0bc74ca67fb6ced80072e0c1fa8
naze <rwt1226@naver.com>
2021-11-30 23:59:04 +0900

    viewBinding 테스트

--- app/src/main/java/com/naze/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/naze/kotlin_practice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.naze.kotlin_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naze.kotlin_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        //viewBinding 설정
+        binding.btnSay.setOnClickListener{ //아이디가 btnSay인 버튼
+            binding.textSay.text = "Hello Kotlin!"
+        }
+
+
     }
 }
